bc59a99905bb6ffae58909d6e338317fe5fc4de7
Winnie <winnieimafidon1995@gmail.com>
2021-10-22 16:27:10 +0700

    Test Part 1

---
+++ script.js
@@ -0,0 +1,56 @@
+// Exercise37
+
+// for (let i = 3; i < 5; i++ ) {
+//   console.log(i);
+// }
+
+// //Excerise38
+
+
+// for (let i = 3; i < 5; ) {
+//   console.log(i);
+//   i += 1;
+// }
+
+//Excerise39 
+
+// let i = 3
+
+// for (; ;) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// Excerise40
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i)
+// }
+
+// const data = [
+//     {
+//       id: 1,
+//       name: "A",
+//       age: 20,
+//     },
+//     {
+//       id: 2,
+//       name: "B",
+//       age: 21,
+//     },
+//     {
+//       id: 3,
+//       name: "C",
+//       age: 22,
+//     },
+// ];
+  
+
+//Excerise41
+
+// data2 = data.filter(value => value.age === 21)
+
+// console.log(data2)
+
+//Excerise42
+
